68c2749013dbe17da458abafe38eb6dab651aada
Angga Nurhiman <nurhimanangga@gmail.com>
2025-11-02 09:30:11 +0700

    :heavy_plus_sign: Setup Quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +12,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
 
 app.mount('#app')
