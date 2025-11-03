9a48878e25cd4c494cc7fdb834ffc6e347bee5ae
Angga Nurhiman <nurhimanangga@gmail.com>
2025-11-03 22:56:27 +0700

    :art: feat(Create sales): Fix mobile view

--- src/components/NewSaleDialog.vue
+++ src/components/NewSaleDialog.vue
@@ -4,7 +4,7 @@
     @update:model-value="$emit('update:modelValue', $event)"
     persistent
   >
-    <q-card style="min-width: 500px">
+    <q-card style="width: 90vw; max-width: 500px">
       <q-card-section class="row items-center q-pb-none">
         <div class="text-h6">Create New Sale</div>
         <q-space />
